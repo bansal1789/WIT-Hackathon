5c5e5a6200c583309242c23fc442307b671348fd
bansal1789 <bansalshruti171027@gmail.com>
2021-02-12 11:35:44 +0530

    first commit

---
+++ react/src/core/theme.js
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'react-native-paper'
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    text: '#f08080',
+    primary: '#f08080',
+    secondary: '#414757',
+    error: '#f13a59',
+  },
+}
